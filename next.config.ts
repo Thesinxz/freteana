4fe465bdef83b3b060bf6a1ecf90a6ab484f4bd6
thesinxz <thesinxz@gmail.com>
2026-04-30 20:59:31 -0400

    fix: remove next-pwa causing infinite redirect loop on production assets

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig: NextConfig = {
   turbopack: {},
   output: "standalone",
@@ -15,4 +8,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
